64d5746d6ec80e510803159d70cb73f526609fc6
xuyy21 <360329045@qq.com>
2024-12-07 23:21:05 +0800

    树人：贴图未完成

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Treants.java
@@ -0,0 +1,163 @@
+package com.shatteredpixel.shatteredpixeldungeon.actors.mobs;
+
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.Challenges;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Cripple;
+import com.shatteredpixel.shatteredpixeldungeon.effects.Chains;
+import com.shatteredpixel.shatteredpixeldungeon.effects.Effects;
+import com.shatteredpixel.shatteredpixeldungeon.effects.Pushing;
+import com.shatteredpixel.shatteredpixeldungeon.items.Generator;
+import com.shatteredpixel.shatteredpixeldungeon.items.Item;
+import com.shatteredpixel.shatteredpixeldungeon.items.food.Mushroom;
+import com.shatteredpixel.shatteredpixeldungeon.mechanics.Ballistica;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.GuardSprite;
+import com.watabou.noosa.audio.Sample;
+import com.watabou.utils.Bundle;
+import com.watabou.utils.Callback;
+import com.watabou.utils.Random;
+
+public class Treants extends Mob{
+
+    private int chainsUsed = 0;
+
+    {
+        spriteClass = GuardSprite.class;
+
+        HP = HT = 50;
+        defenseSkill = 12;
+        baseSpeed = 0.5f;
+
+        EXP = 8;
+        maxLvl = 16;
+
+//        loot = Generator.Category.ARMOR;
+//        lootChance = 0.2f; //by default, see lootChance()
+//        food = new Mushroom();
+//        foodChance = 0.4f;
+
+        properties.add(Property.PLANT);
+        properties.add(Property.LARGE);
+
+        HUNTING = new Treants.Hunting();
+    }
+
+    @Override
+    public int damageRoll() {
+        return Random.NormalIntRange(10, 20);
+    }
+
+    private boolean chain(int target){
+        if (chainsUsed>0 || enemy.properties().contains(Property.IMMOVABLE))
+            return false;
+
+        Ballistica chain = new Ballistica(pos, target, Ballistica.PROJECTILE);
+
+        if (chain.collisionPos != enemy.pos
+                || chain.path.size() < 2
+                || Dungeon.level.pit[chain.path.get(1)])
+            return false;
+        else {
+            int newPos = -1;
+            for (int i : chain.subPath(1, chain.dist)){
+                if (!Dungeon.level.solid[i] && Actor.findChar(i) == null){
+                    newPos = i;
+                    break;
+                }
+            }
+
+            if (newPos == -1){
+                return false;
+            } else {
+                final int newPosFinal = newPos;
+                this.target = newPos;
+
+                if (sprite.visible || enemy.sprite.visible) {
+//                    yell(Messages.get(this, "scorpion"));
+                    new Item().throwSound();
+                    Sample.INSTANCE.play(Assets.Sounds.CHAINS);
+                    sprite.parent.add(new Chains(sprite.center(),
+                            enemy.sprite.destinationCenter(),
+                            Effects.Type.CHAIN,
+                            new Callback() {
+                                public void call() {
+                                    Actor.add(new Pushing(enemy, enemy.pos, newPosFinal, new Callback() {
+                                        public void call() {
+                                            pullEnemy(enemy, newPosFinal);
+                                        }
+                                    }));
+                                    next();
+                                }
+                            }));
+                } else {
+                    pullEnemy(enemy, newPos);
+                }
+            }
+        }
+        chainsUsed = 10;
+        return true;
+    }
+
+    private void pullEnemy(Char enemy, int pullPos ){
+        enemy.pos = pullPos;
+        enemy.sprite.place(pullPos);
+        Dungeon.level.occupyCell(enemy);
+//        Cripple.prolong(enemy, Cripple.class, 4f);
+        if (enemy == Dungeon.hero) {
+            Dungeon.hero.interrupt();
+            Dungeon.observe();
+            GameScene.updateFog();
+        }
+    }
+
+    @Override
+    public int attackSkill( Char target ) {
+        return 20;
+    }
+
+    @Override
+    public int drRoll() {
+        return super.drRoll() + Random.NormalIntRange(2, 8);
+    }
+
+    private final String CHAINSUSED = "chainsused";
+
+    @Override
+    public void storeInBundle(Bundle bundle) {
+        super.storeInBundle(bundle);
+        bundle.put(CHAINSUSED, chainsUsed);
+    }
+
+    @Override
+    public void restoreFromBundle(Bundle bundle) {
+        super.restoreFromBundle(bundle);
+        chainsUsed = bundle.getInt(CHAINSUSED);
+    }
+
+    private class Hunting extends Mob.Hunting{
+        @Override
+        public boolean act( boolean enemyInFOV, boolean justAlerted ) {
+            enemySeen = enemyInFOV;
+
+            if (Dungeon.isChallenged(Challenges.CRAZY_PLANT)
+                    && chainsUsed<=0
+                    && enemyInFOV
+                    && !isCharmedBy( enemy )
+                    && !canAttack( enemy )
+                    && Dungeon.level.distance( pos, enemy.pos ) < 5
+
+                    && chain(enemy.pos)){
+                return !(sprite.visible || enemy.sprite.visible);
+            } else {
+                chainsUsed--;
+                if (chainsUsed < 0) chainsUsed = 0;
+                return super.act( enemyInFOV, justAlerted );
+            }
+
+        }
+    }
+}

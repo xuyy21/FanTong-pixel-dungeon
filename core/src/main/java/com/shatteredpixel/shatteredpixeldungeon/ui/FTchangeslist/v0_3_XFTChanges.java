71a6a361f03ecd9cf8fbe82c3c35d97f35feab39
xuyy21 <360329045@qq.com>
2025-01-06 15:57:13 +0800

    更新改动说明

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/FTchangeslist/v0_3_XFTChanges.java
@@ -0,0 +1,44 @@
+package com.shatteredpixel.shatteredpixeldungeon.ui.FTchangeslist;
+
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.ChangesScene;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
+import com.shatteredpixel.shatteredpixeldungeon.ui.Window;
+import com.shatteredpixel.shatteredpixeldungeon.ui.changelist.ChangeButton;
+import com.shatteredpixel.shatteredpixeldungeon.ui.changelist.ChangeInfo;
+import com.watabou.noosa.Image;
+
+import java.util.ArrayList;
+
+public class v0_3_XFTChanges {
+    public static void addAllChanges( ArrayList<ChangeInfo> changeInfos ){
+        add_v0_3_0_Changes(changeInfos);
+    }
+
+    public static void add_v0_3_0_Changes(ArrayList<ChangeInfo> changeInfos ){
+
+        ChangeInfo changes = new ChangeInfo("v0.3.0", true, "");
+        changes.hardlight(Window.TITLE_COLOR);
+        changeInfos.add(changes);
+
+        changes = new ChangeInfo(Messages.get(ChangesScene.class, "new"), false, null);
+        changes.hardlight(Window.TITLE_COLOR);
+        changeInfos.add(changes);
+
+        changes = new ChangeInfo(Messages.get(ChangesScene.class, "changes"), false, null);
+        changes.hardlight(CharSprite.WARNING);
+        changeInfos.add(changes);
+
+        changes.addButton(new ChangeButton(new Image(Assets.Sprites.SPINNER, 144, 0, 16, 16), Messages.get(ChangesScene.class, "bugfixes"),
+                "诅咒的魔戒怪爪的诅咒效果触发概率从10%下调回1%，这是由于测试过程中的疏漏导致的非常抱歉\n"));
+
+        changes = new ChangeInfo(Messages.get(ChangesScene.class, "buffs"), false, null);
+        changes.hardlight(CharSprite.POSITIVE);
+        changeInfos.add(changes);
+
+        changes = new ChangeInfo(Messages.get(ChangesScene.class, "nerfs"), false, null);
+        changes.hardlight(CharSprite.NEGATIVE);
+        changeInfos.add(changes);
+    }
+}

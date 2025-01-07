7cdb6f42811b9c4a04838a3f70a2e32fcd456adb
xuyy21 <360329045@qq.com>
2025-01-07 18:51:21 +0800

    V300先导测试版 去掉INDEV编号

--- SPD-classes/src/main/java/com/watabou/utils/DeviceCompat.java
+++ SPD-classes/src/main/java/com/watabou/utils/DeviceCompat.java
@@ -65,7 +65,8 @@ public class DeviceCompat {
 	}
 	
 	public static boolean isDebug(){
-		return Game.version.contains("INDEV");
+//		return Game.version.contains("INDEV");
+		return true;
 	}
 	
 	public static void log( String tag, String message ){

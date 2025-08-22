d7a155597fc971f4c96e5be0fb4503d27aa12ca5
xuyy21 <360329045@qq.com>
2025-08-22 21:33:19 +0800

    完成售货机的代码

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/VendingMachine.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/VendingMachine.java
@@ -5,8 +5,16 @@ import com.shatteredpixel.shatteredpixeldungeon.Challenges;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.items.InsulatedGloves;
 import com.shatteredpixel.shatteredpixeldungeon.items.Item;
+import com.shatteredpixel.shatteredpixeldungeon.items.MagicMonocle;
 import com.shatteredpixel.shatteredpixeldungeon.items.food.ChangFen;
+import com.shatteredpixel.shatteredpixeldungeon.items.food.Chocolate_Egg;
+import com.shatteredpixel.shatteredpixeldungeon.items.food.IcyRedTea;
+import com.shatteredpixel.shatteredpixeldungeon.items.food.Kiwi_Fruit;
+import com.shatteredpixel.shatteredpixeldungeon.items.food.LaTiao;
+import com.shatteredpixel.shatteredpixeldungeon.items.food.Popsicle;
+import com.shatteredpixel.shatteredpixeldungeon.items.food.SleepCandy;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
@@ -15,14 +23,28 @@ import com.shatteredpixel.shatteredpixeldungeon.sprites.VendingMachineSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.XuyySprite;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
 import com.shatteredpixel.shatteredpixeldungeon.windows.WndOptions;
+import com.sun.tools.javac.code.Attribute;
 import com.watabou.noosa.Game;
 import com.watabou.noosa.Image;
+import com.watabou.utils.Bundlable;
 import com.watabou.utils.Bundle;
 import com.watabou.utils.Callback;
+import com.watabou.utils.Random;
+import com.watabou.utils.Reflection;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import sun.security.util.Length;
 
 public class VendingMachine extends NPC{
+
+    public ArrayList<Class<? extends Item>> T1_GOODS = new ArrayList<>();
+    public ArrayList<Class<? extends Item>> T2_GOODS = new ArrayList<>();
+    public ArrayList<Class<? extends Item>> T3_GOODS = new ArrayList<>();
+    public ArrayList<Class<? extends Item>> T4_GOODS = new ArrayList<>();
 
     {
         spriteClass = VendingMachineSprite.class;
@@ -109,15 +131,64 @@ public class VendingMachine extends NPC{
         return sprite;
     }
 
-    public static ArrayList<Item> getGoods() {
+    public void init_Goods() {
+        int challenges = Challenges.activeChallenges();
+
+        if (challenges>=1 && T1_GOODS.isEmpty()) {
+            T1_GOODS.add(ChangFen.class);
+            T1_GOODS.add(Chocolate_Egg.class);
+//            Random.shuffle(T1_GOODS);
+        }
+        if (challenges>=4 && T2_GOODS.isEmpty()) {
+            T2_GOODS.add(IcyRedTea.class);
+            T2_GOODS.add(Kiwi_Fruit.class);
+            T2_GOODS.add(Popsicle.class);
+            Random.shuffle(T2_GOODS);
+        }
+        if (challenges>=7 && T3_GOODS.isEmpty()) {
+            T3_GOODS.add(SleepCandy.class);
+            T3_GOODS.add(LaTiao.class);
+//            Random.shuffle(T3_GOODS);
+        }
+        if (challenges>=10 && T4_GOODS.isEmpty()) {
+            T4_GOODS.add(MagicMonocle.class);
+            T4_GOODS.add(InsulatedGloves.class);
+//            Random.shuffle(T4_GOODS);
+        }
+    }
+
+    public ArrayList<Item> getGoods() {
         ArrayList<Item> goods = new ArrayList<>();
 
-        goods.add(new ChangFen());
+        init_Goods();
+
+        if (!T1_GOODS.isEmpty()) {
+            goods.add(Reflection.newInstance(T1_GOODS.get(0)));
+            goods.add(Reflection.newInstance(T1_GOODS.get(1)));
+        }
+        if (!T2_GOODS.isEmpty()) {
+            goods.add(Reflection.newInstance(T2_GOODS.get(0)));
+            goods.add(Reflection.newInstance(T2_GOODS.get(1)));
+        }
+        if (!T3_GOODS.isEmpty()) {
+            goods.add(Reflection.newInstance(T3_GOODS.get(0)));
+            goods.add(Reflection.newInstance(T3_GOODS.get(1)));
+        }
+        if (!T4_GOODS.isEmpty()) {
+            goods.add(Reflection.newInstance(T4_GOODS.get(0)));
+            goods.add(Reflection.newInstance(T4_GOODS.get(1)));
+        }
+
+        // init Chocolate Egg
+        for (Item i: goods) {
+            if (i instanceof Chocolate_Egg)
+                ((Chocolate_Egg)i).init();
+        }
 
         return goods;
     }
 
-    public static String[] getOptions() {
+    public String[] getOptions() {
         ArrayList<String> options = new ArrayList<>();
         ArrayList<Item> goods = getGoods();
         int valueMultiplier = 10 + Dungeon.scalingDepth() / 5 * 5;
@@ -130,26 +201,56 @@ public class VendingMachine extends NPC{
     }
 
     public static int max_Buy_Counts() {
-        return 100;
+        int challenges = Challenges.activeChallenges();
 
-//        int challenges = Challenges.activeChallenges();
-//
-//        if (challenges < 1) {
-//            return 0;
-//        } else return 3 + (challenges-1) / 3;
+        if (challenges < 1) {
+            return 0;
+        } else return 3 + (challenges-1) / 3;
     }
 
     public static final String COUNTS = "counts";
+    public static final String T1 = "t1";
+    public static final String T2 = "t2";
+    public static final String T3 = "t3";
+    public static final String T4 = "t4";
 
     @Override
     public void storeInBundle(Bundle bundle) {
         super.storeInBundle(bundle);
         bundle.put(COUNTS, buy_counts);
+        bundle.put(T1, T1_GOODS.toArray(new Class[0]));
+        bundle.put(T2, T2_GOODS.toArray(new Class[0]));
+        bundle.put(T3, T3_GOODS.toArray(new Class[0]));
+        bundle.put(T4, T4_GOODS.toArray(new Class[0]));
     }
 
     @Override
     public void restoreFromBundle(Bundle bundle) {
         super.restoreFromBundle(bundle);
         buy_counts = bundle.getInt(COUNTS);
+        if (bundle.getClassArray(T1) != null){
+            for (Class<? extends Item> goods : bundle.getClassArray(T1)) {
+                if (goods != null)
+                    T1_GOODS.add(goods);
+            }
+        }
+        if (bundle.getClassArray(T2) != null){
+            for (Class<? extends Item> goods : bundle.getClassArray(T2)) {
+                if (goods != null)
+                    T2_GOODS.add(goods);
+            }
+        }
+        if (bundle.getClassArray(T3) != null){
+            for (Class<? extends Item> goods : bundle.getClassArray(T3)) {
+                if (goods != null)
+                    T3_GOODS.add(goods);
+            }
+        }
+        if (bundle.getClassArray(T4) != null){
+            for (Class<? extends Item> goods : bundle.getClassArray(T4)) {
+                if (goods != null)
+                    T4_GOODS.add(goods);
+            }
+        }
     }
 }

aa5f9452e8cb8f8e8b20e16ef31953d5dbe5d132
xuyy21 <360329045@qq.com>
2025-08-25 23:33:12 +0800

    copy牧师法术的图标

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/runes/spells/Spell.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/runes/spells/Spell.java
@@ -2,6 +2,7 @@ package com.shatteredpixel.shatteredpixeldungeon.runes.spells;
 
 import static com.shatteredpixel.shatteredpixeldungeon.runes.Runes.RUNES_NUM;
 
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Invisibility;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.MagicImmune;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
@@ -29,6 +30,36 @@ public abstract class Spell {
     public static int SPELLICON = 8;
     public static int DEFAULT   = SPELLICON+0;
 
+    //holy spells
+    public static int HOLY_SPELLICON        = SPELLICON+8;
+    public static final int GUIDING_LIGHT   = HOLY_SPELLICON+0;
+    public static final int HOLY_WEAPON     = HOLY_SPELLICON+1;
+    public static final int HOLY_WARD       = HOLY_SPELLICON+2;
+    public static final int HOLY_INTUITION  = HOLY_SPELLICON+3;
+    public static final int SHIELD_OF_LIGHT = HOLY_SPELLICON+4;
+    public static final int RECALL_GLYPH    = HOLY_SPELLICON+5;
+    public static final int SUNRAY          = HOLY_SPELLICON+6;
+    public static final int DIVINE_SENSE    = HOLY_SPELLICON+7;
+    public static final int BLESS           = HOLY_SPELLICON+8;
+    public static final int CLEANSE         = HOLY_SPELLICON+9;
+    public static final int RADIANCE        = HOLY_SPELLICON+10;
+    public static final int HOLY_LANCE      = HOLY_SPELLICON+11;
+    public static final int HALLOWED_GROUND = HOLY_SPELLICON+12;
+    public static final int MNEMONIC_PRAYER = HOLY_SPELLICON+13;
+    public static final int SMITE           = HOLY_SPELLICON+14;
+    public static final int LAY_ON_HANDS    = HOLY_SPELLICON+15;
+    public static final int AURA_OF_PROTECTION = HOLY_SPELLICON+16;
+    public static final int WALL_OF_LIGHT   = HOLY_SPELLICON+17;
+    public static final int DIVINE_INTERVENTION = HOLY_SPELLICON+18;
+    public static final int JUDGEMENT       = HOLY_SPELLICON+19;
+    public static final int FLASH           = HOLY_SPELLICON+20;
+    public static final int BODY_FORM       = HOLY_SPELLICON+21;
+    public static final int MIND_FORM       = HOLY_SPELLICON+22;
+    public static final int SPIRIT_FORM     = HOLY_SPELLICON+23;
+    public static final int BEAMING_RAY     = HOLY_SPELLICON+24;
+    public static final int LIFE_LINK       = HOLY_SPELLICON+25;
+    public static final int STASIS          = HOLY_SPELLICON+26;
+
     public Image icon() {
         return new RuneIcon(icon);
     }
@@ -38,11 +69,11 @@ public abstract class Spell {
     }
 
     public String desc() {
-        return Messages.get(this, "desc") + "\n\n" + Messages.get(this, "overrunes", (int)overRunes());
+        return Messages.get(this, "desc") + "\n\n" + Messages.get(this, "overrunes", (int)overRunes(Dungeon.hero));
     }
 
     public String shortDesc() {
-        return Messages.get(this, "shortdesc" + " " + Messages.get(this, "overrunes", (int)overRunes()));
+        return Messages.get(this, "shortdesc" + " " + Messages.get(this, "overrunes", (int)overRunes(Dungeon.hero)));
     }
 
     public boolean canCast(Implement implement, Hero hero) {
@@ -59,7 +90,7 @@ public abstract class Spell {
 
     public abstract void onCast(Implement implement, Hero hero);
 
-    public float overRunes() {
+    public float overRunes(Hero hero) {
         return 10f + 10f*tier;
     }
 

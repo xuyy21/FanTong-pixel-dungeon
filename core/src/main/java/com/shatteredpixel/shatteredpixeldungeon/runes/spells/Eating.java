0e6258d21dd3dafd32ed1e83ae4f665a3b8ea515
xuyy21 <360329045@qq.com>
2025-09-01 23:02:03 +0800

    进食秘法补上时间消耗

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/runes/spells/Eating.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/runes/spells/Eating.java
@@ -15,8 +15,8 @@ public class Eating extends Spell{
 
     @Override
     public void onCast(Implement implement, Hero hero){
-        Talent.onFoodEaten(hero, 100*implement.powerMultiplier(hero, this), null);
         hero.sprite.operate(hero.pos);
+        Talent.onFoodEaten(hero, 100*implement.powerMultiplier(hero, this), null);
         hero.spendAndNext(implement.delay(hero, this));
     }
 }

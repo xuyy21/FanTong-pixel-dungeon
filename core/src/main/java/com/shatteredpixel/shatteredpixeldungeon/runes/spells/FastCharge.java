27dec297b7e175f12b368b5137811fa67b74b18a
xuyy21 <360329045@qq.com>
2026-01-06 10:58:57 +0800

    闪电快充

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/runes/spells/FastCharge.java
@@ -0,0 +1,53 @@
+package com.shatteredpixel.shatteredpixeldungeon.runes.spells;
+
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.items.Item;
+import com.shatteredpixel.shatteredpixeldungeon.items.implement.Implement;
+import com.shatteredpixel.shatteredpixeldungeon.items.wands.Wand;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.MagesStaff;
+import com.watabou.noosa.audio.Sample;
+
+public class FastCharge extends InventorySpell{
+    public static FastCharge INSTANCE = new FastCharge();
+
+    {
+        type = TYPE.ENERGETIC;
+        icon = WANDS_CHARGE;
+        tier = 3;
+    }
+
+    @Override
+    protected boolean usableOnItem(Item item) {
+        return item instanceof Wand || item instanceof MagesStaff;
+    }
+
+    @Override
+    protected void onItemSelected(Implement implement, Hero hero, Item item ) {
+        if (item == null){
+            return;
+        }
+
+        float charge = implement.powerMultiplier(hero, this);
+
+        for (Item i: hero.belongings.backpack.items) {
+            if (i instanceof Wand) {
+                ((Wand) i).gainCharge(charge, true);
+            } else if (i instanceof MagesStaff) {
+                ((MagesStaff) i).gainCharge(charge, true);
+            }
+        }
+
+        if (item instanceof Wand) {
+            ((Wand) item).gainCharge(charge, true);
+        } else if (item instanceof MagesStaff) {
+            ((MagesStaff) item).gainCharge(charge, true);
+        }
+
+        hero.busy();
+        hero.sprite.operate(hero.pos);
+        Sample.INSTANCE.play( Assets.Sounds.CHARGEUP );
+        hero.spendAndNext( implement.delay(hero, this) );
+        onSpellCast(implement, hero);
+    }
+}

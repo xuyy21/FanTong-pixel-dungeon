fbef02f7be6d186104fbf5ba6b31819192ad295e
xuyy21 <360329045@qq.com>
2026-01-07 16:50:15 +0800

    压胜魔咒

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/runes/spells/Suppress.java
@@ -0,0 +1,48 @@
+package com.shatteredpixel.shatteredpixeldungeon.runes.spells;
+
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Hex;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Vulnerable;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Weakness;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.items.implement.Implement;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
+
+public class Suppress extends TargetedSpell{
+    public static Suppress INSTANCE = new Suppress();
+
+    public static float Duration = 8f;
+
+    {
+        type = TYPE.PHYSICAL;
+        icon = SUPPRESS;
+        tier = 2;
+    }
+
+    @Override
+    protected void onTargetSelected(Implement implement, Hero hero, Integer target) {
+        if (target == null){
+            return;
+        }
+
+        if (!Dungeon.level.heroFOV[target] || Actor.findChar(target)==null){
+            GLog.w(Messages.get(this, "no_target"));
+            return;
+        } else {
+            hero.busy();
+            hero.sprite.operate(hero.pos);
+
+            Char ch = Actor.findChar(target);
+            Buff.affect(ch, Vulnerable.class, Duration);
+            Buff.affect(ch, Weakness.class, Duration);
+            Buff.affect(ch, Hex.class, Duration);
+
+            hero.spendAndNext(implement.delay(hero, this));
+            onSpellCast(implement, hero);
+        }
+    }
+}

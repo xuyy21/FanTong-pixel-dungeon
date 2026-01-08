6a805398c6325a60b4fdc40aba2cdbf068666bb3
xuyy21 <360329045@qq.com>
2026-01-08 17:13:04 +0800

    debug:反转伤害buff该为FlavourBuff

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/runes/spells/AbsorbDamage.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/runes/spells/AbsorbDamage.java
@@ -3,6 +3,7 @@ package com.shatteredpixel.shatteredpixeldungeon.runes.spells;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.FlavourBuff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.effects.FloatingText;
 import com.shatteredpixel.shatteredpixeldungeon.items.implement.Implement;
@@ -34,14 +35,14 @@ public class AbsorbDamage extends TargetedSpell{
             hero.busy();
             hero.sprite.operate(hero.pos);
 
-            Buff.affect(hero, Absorbing.class).setLeft(2f*implement.powerMultiplier(hero, this));
+            Buff.affect(hero, Absorbing.class, 2f*implement.powerMultiplier(hero, this));
 
             hero.spendAndNext(implement.delay(hero, this));
             onSpellCast(implement, hero);
         }
     }
 
-    public static class Absorbing extends Buff {
+    public static class Absorbing extends FlavourBuff {
         {
             type = buffType.NEUTRAL;
             announced = true;
@@ -49,23 +50,19 @@ public class AbsorbDamage extends TargetedSpell{
 
         private static final float DURATION = 2f;
 
-        private float left = 0f;
         private float damage = 0f;
 
-        private static final String LEFT	= "left";
         private static final String DAMAGE	= "damage";
 
         @Override
         public void storeInBundle( Bundle bundle ) {
             super.storeInBundle( bundle );
-            bundle.put( LEFT, left );
             bundle.put( DAMAGE, damage );
         }
 
         @Override
         public void restoreFromBundle( Bundle bundle ) {
             super.restoreFromBundle(bundle);
-            left = bundle.getFloat( LEFT );
             damage = bundle.getFloat( DAMAGE );
         }
 
@@ -73,18 +70,6 @@ public class AbsorbDamage extends TargetedSpell{
             float toAbsorb = damage * 0.8f;
             this.damage += toAbsorb;
             return damage - toAbsorb;
-        }
-
-        public void setLeft(float left) {
-            if (left>this.left) this.left = left;
-        }
-
-        public float getLeft() {
-            return left;
-        }
-
-        public void extend(float extension) {
-            left += extension;
         }
 
         @Override
@@ -105,17 +90,7 @@ public class AbsorbDamage extends TargetedSpell{
 
         @Override
         public float iconFadePercent() {
-            return Math.max(0, (DURATION - left) / DURATION);
-        }
-
-        @Override
-        public String iconTextDisplay() {
-            return Integer.toString((int)left);
-        }
-
-        @Override
-        public String desc() {
-            return Messages.get(this, "desc", dispTurns(left));
+            return Math.max(0, (DURATION - visualcooldown()) / DURATION);
         }
     }
 }

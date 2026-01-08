23e9c16e78fcef0419e3c00691672e51eb49f32d
xuyy21 <360329045@qq.com>
2026-01-08 21:42:44 +0800

    debug:高能法环的文本问题

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/implement/EnergrticRing.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/implement/EnergrticRing.java
@@ -4,6 +4,7 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.runes.spells.Spell;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 
+// i spell this name wrongly, but change it will cause this item disappear, so let it wrong
 public class EnergrticRing extends Implement{
     {
         image = ItemSpriteSheet.IMPLEMENT_RING;

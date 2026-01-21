319c5cc04f417835683ef928f9a3ab1d07f7010c
xuyy21 <360329045@qq.com>
2026-01-21 17:01:31 +0800

    更新日志

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/FTchangeslist/v0_4_X_FTChanges.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/FTchangeslist/v0_4_X_FTChanges.java
@@ -6,6 +6,7 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.hero.HeroClass;
 import com.shatteredpixel.shatteredpixeldungeon.effects.BadgeBanner;
 import com.shatteredpixel.shatteredpixeldungeon.items.artifacts.CloakOfShadows;
 import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.exotic.ScrollOfDivination;
+import com.shatteredpixel.shatteredpixeldungeon.items.spells.Remove_Missiles_Curse;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.EX_enchantments.RockGuarding;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.runes.spells.Swap_Between;
@@ -267,7 +268,10 @@ public class v0_4_X_FTChanges {
         changes.addButton(new ChangeButton(new ItemSprite(ItemSpriteSheet.IMPLEMENT_BOOK), "符文与符术调整",
                 "法器的试验界面添加随机试验功能，点击即可随机选择一个符文组合进行试验。"));
 
-        changes.addButton(new ChangeButton(BadgeBanner.show(Badges.Badge.DEATH_FROM_MIX_GAS.image), "混合毒气徽章",
+        changes.addButton(new ChangeButton(BadgeBanner.image(Badges.Badge.DEATH_FROM_MIX_GAS.image), "混合毒气徽章",
                 "调整混合毒气徽章的获取逻辑，此前的判断逻辑过于严苛导致一些本该获得徽章的情况未能获得徽章。"));
+
+        changes.addButton(new ChangeButton(new ItemSprite(ItemSpriteSheet.REMOVE_MISSILES_CURSE), "投武驱邪菱晶",
+                "加入一种用驱邪卷轴制作的菱晶，它只能对投武生效，但是产量为3。我希望它可以帮助玩家减轻投武系统改革后大多投武需要驱邪的压力。"));
     }
 }

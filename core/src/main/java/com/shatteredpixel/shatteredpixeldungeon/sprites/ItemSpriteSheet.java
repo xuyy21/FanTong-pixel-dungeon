743f23eba50383ee45e5dca3e154a5240743a900
xuyy21 <360329045@qq.com>
2026-06-19 10:29:38 +0800

    新增了一口酥、鲜肉酿、蜜糖箭头的贴图

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ItemSpriteSheet.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/ItemSpriteSheet.java
@@ -848,7 +848,7 @@ public class ItemSpriteSheet {
 	public static final int BAT_COOKIE 		= COOKED_FOOD+27;
 	public static final int CRISPBITE		= COOKED_FOOD+28;
 	public static final int STUFFED_MEAT 	= COOKED_FOOD+29;
-	public static final int HoneyArrow 		= COOKED_FOOD+30;
+	public static final int HONEYARROW 		= COOKED_FOOD+30;
 	static{
 		assignItemRect(Honey_MEAT,    	15, 11);
 		assignItemRect(ICECREAM, 		10, 16);

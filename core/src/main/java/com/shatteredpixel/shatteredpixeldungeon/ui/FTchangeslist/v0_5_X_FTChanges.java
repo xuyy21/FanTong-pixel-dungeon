8ec14946bbefb586598aea1e2a47ce99b3a7b93a
xuyy21 <360329045@qq.com>
2026-07-18 16:30:24 +0800

    更新日志

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/FTchangeslist/v0_5_X_FTChanges.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/FTchangeslist/v0_5_X_FTChanges.java
@@ -2,12 +2,15 @@ package com.shatteredpixel.shatteredpixeldungeon.ui.FTchangeslist;
 
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.HeroSubClass;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.PrisonWarden;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Thief;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.runes.spells.Animate_Armor;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.ChangesScene;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.DM200Sprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.RatKingSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ThiefSprite;
 import com.shatteredpixel.shatteredpixeldungeon.ui.HeroIcon;
 import com.shatteredpixel.shatteredpixeldungeon.ui.Window;
@@ -19,9 +22,29 @@ import java.util.ArrayList;
 
 public class v0_5_X_FTChanges {
     public static void addAllChanges( ArrayList<ChangeInfo> changeInfos ) {
+        add_v0_5_3_Changes(changeInfos);
         add_v0_5_2_Changes(changeInfos);
         add_v0_5_1_Changes(changeInfos);
         add_v0_5_0_Changes(changeInfos);
+    }
+
+    public static void add_v0_5_3_Changes(ArrayList<ChangeInfo> changeInfos ) {
+        ChangeInfo changes = new ChangeInfo("v0.5.3", true, "");
+        changes.hardlight(Window.TITLE_COLOR);
+        changeInfos.add(changes);
+
+        changes.addButton( new ChangeButton(new Image(new RatKingSprite()), "鼠王BOSS",
+                "从现在开始，下水道区域尽头的BOSS有50%的概率被鼠王替换。\n\n鼠王本身没有战斗能力，但是你也无法直接伤害它，在处理源源不断的鼠王卫队的同时来一场\"躲鼠鼠\"游戏吧。\n也记得寻找5个被藏起来的\"鼠王宝藏\"。"));
+
+        changes.addButton( new ChangeButton(new Image(new PrisonWarden()), "典狱长BOSS",
+                "从现在开始，监狱区域尽头的BOSS有50%的概率被典狱长替换。\n\n典狱长只是在恪守自己的职责，镇压腐化的囚徒，你未必需要与她斗个你死我活。在典狱长的生命值低于一半后进入二阶段，此时她将不再是你的敌人。"));
+
+        changes.addButton( new ChangeButton(new Image(new Animate_Armor.Animated_Armor()), "活化护甲",
+                "活化护甲被削弱，攻击力降低。"));
+
+        changes.addButton( new ChangeButton(new ItemSprite(ItemSpriteSheet.RING_HOLDER), "韧性戒指",
+                "韧性戒指得到加强，现在可以在损失90.91%的生命值时就可以取得最大效果。"));
+
     }
 
     public static void add_v0_5_2_Changes(ArrayList<ChangeInfo> changeInfos ) {
